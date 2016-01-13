491de228dc8448790b509855768fcea5db5f80f3
hanxiao <artex.xh@gmail.com>
2016-01-13 22:02:51 +0100

    UPDATE page

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by hxiao on 16/1/13.
+ */
+$(document).ready(function() {
+    //carousel options
+    $('#quote-carousel').carousel({
+        pause: true, interval: 10000,
+    });
+});
